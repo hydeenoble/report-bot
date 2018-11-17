8814a677e857983b4643401063212e3467418b7a
EMEHINOLA Idowu <hydeenoble39@gmail.com>
2018-11-17 21:21:14 +0100

    Implented the 'next', 'block' and 'in-progress' commands

--- service/message.js
+++ service/message.js
@@ -5,9 +5,24 @@ class MessageService {
     }
 
     done(messagePayload){
-        this.logger.info('Sending done response message...')
+        this.logger.info('Sending done response message...');
         this.bot.postMessage(messagePayload.channel, 
             'Nice job! Enter another accomplishment for this week using `done`, type `next` to add a goal for next week or use `block` to note something that’s standing in your way.');
+    }
+    
+    next(messagePayload){
+        this.logger.info('Sending \'next\' category response message...');
+        this.bot.postMessage(messagePayload.channel, 'Place holder messages for `next` category');
+    }
+    inProgress(messagePayload){
+        this.logger.info('Sending \'in-progress\' category response message...');
+        this.bot.postMessage(messagePayload.channel, 'Place holder messages for `in-progress` category');
+
+
+    }
+    block(messagePayload){
+        this.logger.info('Sending \'block\' category response message...')
+        this.bot.postMessage(messagePayload.channel, 'Place holder messages for `block` category');
     }
 }
 
